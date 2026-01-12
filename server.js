188c8f42a48305b281cb04003223c6a9a42f2895
Sohail Shaikh <sohailshaikh062003@gmail.com>
2026-01-12 20:15:55 +0530

    Update server log message to include ellipsis

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     // Connect to MongoDB
     connectDB();
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port} ...!`);
 });
 
 
