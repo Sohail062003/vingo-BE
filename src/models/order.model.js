bd2a4ddd3e61cf2b75c33a2b9a14e2c3bc04b195
Sohail Shaikh <sohailshaikh062003@gmail.com>
2026-02-04 15:24:54 +0530

    Add order model with shopOrder and shopOrderItem schemas

---
+++ src/models/order.model.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+
+const shopOrderItemSchema = new mongoose.Schema({
+    item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item',
+    },
+    price: {
+        type: Number,
+    },
+    quantity: {
+        type: Number,
+    }
+
+}, {timestamps: true});
+
+const shopOrderSchema = new mongoose.Schema({
+    shop: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Shop',
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    subTotal: {
+        type: Number,
+    },
+    shopOrderItems: [shopOrderItemSchema]
+
+
+},{timestamps: true});
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['cod', 'online'],
+        required: true,
+    },   
+    deliveryAddress: {
+        type: String, 
+        latitude: Number,
+        longitude: Number,
+    },
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
+    shopOrder: [shopOrderSchema]
+
+}, { timestamps: true });
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
