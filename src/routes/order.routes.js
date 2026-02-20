246f802beae9282f254a0908dcd91eb39f0ae564
Sohail Shaikh <sohailshaikh062003@gmail.com>
2026-02-21 03:20:11 +0530

    Add getDeliveryBoyAssignment method and route to retrieve delivery assignments

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -6,6 +6,8 @@ import isAuth from '../middlewares/isAuth.js';
 const route = express.Router();
 
 route.get("/my-orders", isAuth ,OrderController.getMyOrders);
+route.get("/get-assignments", isAuth ,OrderController.getDeliveryBoyAssignment);
+
 route.post("/place-order", isAuth ,OrderController.placeOrder);
 route.post("/update-status/:orderId/:shopId", isAuth, OrderController.updateOrderStatus);
 
