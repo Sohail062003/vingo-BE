f60bf92a67dca98ee81d6672d5da78e623bd4dbf
Sohail Shaikh <sohailshaikh062003@gmail.com>
2026-05-16 14:18:56 +0530

    Fix formatting in user schema: add missing comma for location field

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -36,8 +36,8 @@ const userShema = new mongoose.Schema({
     location: {
         type: {type: String, enum:['Point'], default: 'Point'},
         coordinates: {type:[Number], default:[0,0]} // long, latitiude
-    }
-
+    }, 
+    
 }, {timestamps: true});
 
 userShema.index({ location: '2dsphere' })
